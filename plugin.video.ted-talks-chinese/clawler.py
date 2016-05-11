2320e18ce2b10047206217f2b14d747a950c6cf7
Dainese Hsiao <daineseh@gmail.com>
2016-05-11 18:04:07 +0800

    Add classes for process subtitle

--- plugin.video.ted-talks-chinese/clawler.py
+++ plugin.video.ted-talks-chinese/clawler.py
@@ -3,12 +3,75 @@
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 
+import re
 import requests
 import urlparse
 
 
 BASE_URL = 'https://www.ted.com'
 BUILD_URL = lambda x: urlparse.urljoin(BASE_URL, x).encode('utf8')
+
+
+class TEDSub2SubRip:
+    def __init__(self, ted_subs, intro_time = 0):
+        self.ted_subs = ted_subs
+        self.intro_time = intro_time
+        self.statements = []
+        self.__process_statements()
+
+    def __srt_time(self, tst):
+        """Format Time from TED Subtitles format to SRT time Format."""
+        secs, mins, hours = ((tst / 1000) % 60), (tst / 60000), (tst / 3600000)
+        right_srt_time = ("%02d:%02d:%02d,%02d" %
+                           (int(hours), int(mins), int(secs), divmod(secs, 1)[1] * 1000))
+        return right_srt_time
+
+    def __process_statements(self):
+        if not self.ted_subs.has_key('captions'):
+            return
+
+        for line, caption in enumerate(self.ted_subs.get('captions'), start=1):
+            start = caption.get('startTime') + self.intro_time
+            end = caption.get('duration') + start
+            time_line = '%s --> %s' % (self.__srt_time(start), self.__srt_time(end))
+            text_line = caption.get('content').encode('utf8')
+            self.statements.append([str(line), time_line, text_line, '\n'])
+
+    def reads(self):
+        if not self.statements:
+            return ''
+
+        sub_content = ''
+        for stat in self.statements:
+            sub_content += '\n'.join(stat)
+        return sub_content
+
+
+class TEDSub2SubRipWrapper:
+    def __init__(self, url):
+        self.req = requests.get(url)
+        if not self.req:
+            return None
+        self.__process()
+
+    def __process(self):
+        regex_intro = re.compile('"introDuration":(\d+\.?\d+)')
+        regex_id = re.compile('"id":(\d+),')
+
+        talk_id = regex_id.findall(self.req.text)[0]
+        tc_sub_url = urlparse.urljoin('https://www.ted.com/talks/subtitles/id/', '%s/lang/%s' % (talk_id, 'zh-tw'))
+        print(tc_sub_url)
+        req = requests.get(tc_sub_url)
+        if not self.req:
+            return None
+
+        intro_time = ((float(regex_intro.findall(self.req.text)[0]) + 1) * 1000)
+
+        self.sub_obj = TEDSub2SubRip(req.json(), intro_time)
+
+    def output(self):
+        with open('ted_talk_sub.srt', 'w') as fp:
+            fp.write(self.sub_obj.reads())
 
 
 class Talk:
